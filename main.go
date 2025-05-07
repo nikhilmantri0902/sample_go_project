3efe1426993b7cf5e3dec2804f1c4df3d4cd1032
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2025-05-07 18:18:52 +0530

    checking change

--- main.go
+++ main.go
@@ -8,4 +8,10 @@ import (
 func main() {
 	fmt.Println("Come on home!!!")
 	fmt.Println(math.MaxInt)
+	fmt.Println(math.MaxInt8)
+	fmt.Println(math.MaxInt16)
+	fmt.Println(math.MaxInt32)
+	fmt.Println(math.MaxInt64)
+	fmt.Println(math.MaxUint8)
+	fmt.Println(math.MaxUint16)
 }
